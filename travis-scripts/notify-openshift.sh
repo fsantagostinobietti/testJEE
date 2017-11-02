800ad397f7d2d5f05497a6a572cc0a76af9f6f54
fsantagostinobietti <fabio.santagostinobietti@unicredit.eu>
2017-11-02 10:35:09 +0100

    openshift creadentials instead of token

--- travis-scripts/notify-openshift.sh
+++ travis-scripts/notify-openshift.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
-oc login https://api.starter-us-west-2.openshift.com --token=$OPENSHIFT_TOKEN
+#oc login https://api.starter-us-west-2.openshift.com --token=$OPENSHIFT_TOKEN
+oc login https://api.starter-us-west-2.openshift.com --username=$OPENSHIFT_USERNAME  --password=$OPENSHIFT_PASSWORD
 oc import-image testjee --all=true
